b11818424f64e44060d5b3b12d3c587011bda62d
Ultimo User <Ultimo User@Ultimo-Angular>
2015-06-18 11:05:37 -0700

    removed dateSlider and added a drop down field for time

--- public_html/ultimotls/dashboard/treemap/treemapController.js
+++ public_html/ultimotls/dashboard/treemap/treemapController.js
@@ -7,7 +7,13 @@
 var treemapControllerModule = angular.module('treemapControllerModule', ['ultimotls', 'treemapControllerModule', 'ngRoute', 'ngSlider']);
 
 treemapControllerModule.controller('treemapController', ['$scope', 'mongoAggregateService', function($scope, mongoAggregateService){
+    //initialize data
     $scope.toDate=null; $scope.fromDate=null;
+    $scope.timeOptions = [{"time":.25, "description":"15 minutes"},{"time":.5, "description":"30 minutes"},
+                          {"time":1,"description":"1 hour"},{"time":24, "description":"24 hours"},
+                          {"time":48,"description":"48 hours"}];
+    $scope.timeSelected = $scope.timeOptions[2];
+    
     if(!$scope.toDate){
         var currentDateTime = new Date(); 
         $scope.fromDate = new Date(currentDateTime - 7200000).toISOString(); //Current minus 2 hours
@@ -18,23 +24,22 @@ treemapControllerModule.controller('treemapController', ['$scope', 'mongoAggrega
     $scope.sliderDatePromise = mongoAggregateService.callHttp(dataQuery);
     //date slider Config
     $scope.sliderDatePromise = {};
-    $scope.sliderValue = "2";
-    $scope.options = {       
-        from: 1,
-        to: 48,
-        step: 1,
-        dimension: " hours",
-        callback: function(value, elt){
-                    
-            var currentDateTime = new Date();
-            $scope.fromDate = new Date(currentDateTime - (value*60*60*1000)).toISOString();
-            $scope.toDate = new Date(currentDateTime).toISOString();
-            var sliderDataQuery = "[ { '$match': { '$and': [ { 'timestamp': { '$gte': " +
-                    "{'$date': '"+$scope.fromDate+"'}, '$lt': {'$date': '"+ $scope.toDate +"'} } }, { '$and': [ {'severity': {'$ne': null}}, {'severity': {'$exists': true, '$ne': ''}} ] } ] } },{ '$group': { '_id' : { 'interface1': '$interface1', 'application': '$application' }, 'count': {'$sum': 1} } } , { '$group': { '_id' : { 'application': '$_id.application' }, 'data': { '$addToSet':{ 'name': '$_id.interface1', 'size': '$count' } } } } , { '$project': { '_id': 1, 'name': '$_id.application', 'children': '$data' } } ]";
+    //Drop Down Time Selection
+    $scope.fromDateChange = function(){
+        var currentDateTime = new Date();
+        $scope.fromDate = new Date(currentDateTime - ($scope.timeSelected.time*60*60*1000)).toISOString();
+        $scope.toDate = new Date(currentDateTime).toISOString(); 
+        var sliderDataQuery = "[ { '$match': { '$and': [ { 'timestamp': { '$gte': " +
+                    "{'$date': '"+$scope.fromDate+"'}, '$lt': {'$date': '"+ $scope.toDate +"'} } },"+
+                    "{ '$and': [ {'severity': {'$ne': null}}, {'severity': {'$exists': true, '$ne': ''}}"+
+                    "] } ] } },{ '$group': { '_id' : { 'interface1': '$interface1', 'application': '$application'"+
+                    "}, 'count': {'$sum': 1} } } , { '$group': { '_id' : { 'application': '$_id.application' },"+
+                    " 'data': { '$addToSet':{ 'name': '$_id.interface1', 'size': '$count' } } } } , "+
+                    "{ '$project': { '_id': 1, 'name': '$_id.application', 'children': '$data' } } ]";
             
-            $scope.sliderDatePromise = mongoAggregateService.callHttp(sliderDataQuery);
-        }
+        $scope.sliderDatePromise = mongoAggregateService.callHttp(sliderDataQuery);
     };
+    
     $scope.replaceGraph = function(divID, newData, element, newGraph){
         var elementExists = document.getElementById("treemapZoom");
         //edit.removeChild(edit.childNodes[0]);
