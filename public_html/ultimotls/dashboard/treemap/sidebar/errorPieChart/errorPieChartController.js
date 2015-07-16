140d1032a39393925e7bc06f5deb616a2aa2a6bd
Ultimo User <Ultimo User@Ultimo-Angular>
2015-07-16 09:39:41 -0700

    Now creates a query to group results by errorType

--- public_html/ultimotls/dashboard/treemap/sidebar/errorPieChart/errorPieChartController.js
+++ public_html/ultimotls/dashboard/treemap/sidebar/errorPieChart/errorPieChartController.js
@@ -1,7 +1,33 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var errorPieChartControllerModule = angular.module('errorPieChartControllerModule', ['ultimotls', 'auditControllerModule']);
 
-
+errorPieChartControllerModule.controller('errorPieChartController', ['$scope', 'mongoAggregateService', 'treemapSaver', 'auditQuery','queryEnv','timeService',
+    function($scope, mongoAggregateService, treemapSaver, auditQuery, queryEnv, timeService){
+    //service to get current Time
+    var time = timeService.getTime();
+    $scope.toDate = time.toDate;
+    $scope.fromDate = time.fromDate;
+    //service to get current Environment
+    $scope.env = queryEnv.getEnv();
+    
+    $scope.treemapSaver = treemapSaver;
+    $scope.auditQuery = auditQuery; //Don't think i need it, if not remove from injection
+    
+//    need a service to pass timeChange
+    $scope.$on("timeChangeBroadcast", function(){//Listens for Time Change
+        var timeTemp = timeService.getTime();
+        $scope.toDate = timeTemp.toDate;
+        $scope.fromDate = timeTemp.fromDate;
+        $scope.errorPieChartData($scope.fromDate, $scope.toDate);
+    })
+    
+    $scope.$on("envChangeBroadcast", function(){//Listens for Environment Change
+        $scope.env = queryEnv.getEnv();
+        $scope.errorPieChartData($scope.fromDate, $scope.toDate);
+    })
+    $scope.errorPieChartData = function(fromDate, toDate){
+        var sliderDataQuery = "[{\"$match\":{\"$and\":[{\"timestamp\":{\"$gte\":{\"$date\":\""+fromDate+"\"},\"$lt\":{\"$date\":\""+
+            toDate+"\"}}},{\"$and\":[{\"errorType\":{\"$ne\":null}}, {\"errorType\":{\"$exists\":true,\"$ne\":\"\"}},{\"envid\":\""+
+            $scope.env.dbName+"\"}]}]}},{$group:{_id:\"$errorType\", count:{$sum:1}}}]";  
+        $scope.errorPieChartPromise = mongoAggregateService.callHttp(sliderDataQuery);
+    };
+}]);
