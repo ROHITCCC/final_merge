388ec0e05f946be21f604a62ae0f256173059e2e
Ultimo User <Ultimo User@Ultimo-Angular>
2015-07-16 09:40:01 -0700

    removing repetitive code

--- public_html/ultimotls/dashboard/treemap/sidebar/severityPieChart/severitryPieChartController.js
+++ public_html/ultimotls/dashboard/treemap/sidebar/severityPieChart/severitryPieChartController.js
@@ -10,29 +10,29 @@ severityPieChartControllerModule.controller('severityPieChartController', ['$sco
     //service to get current Time
     var time = timeService.getTime();
     $scope.toDate = time.toDate;
-    $scope.fromDate = time.fromDate
+    $scope.fromDate = time.fromDate;
     //service to get current Environment
-    $scope.env = queryEnv.getEnv()
+    $scope.env = queryEnv.getEnv();
     
     $scope.treemapSaver = treemapSaver;
     $scope.auditQuery = auditQuery; //Don't think i need it, if not remove from injection
     
 //    need a service to pass timeChange
     $scope.$on("timeChangeBroadcast", function(){//Listens for Time Change
-        var timeTemp = timeService.getTime()
+        var timeTemp = timeService.getTime();
         $scope.toDate = timeTemp.toDate;
-        $scope.fromDate = timeTemp.fromDate
-        $scope.severityPieChartData($scope.fromDate, $scope.toDate, $scope.env);
+        $scope.fromDate = timeTemp.fromDate;
+        $scope.severityPieChartData($scope.fromDate, $scope.toDate);
     })
     
     $scope.$on("envChangeBroadcast", function(){//Listens for Environment Change
         $scope.env = queryEnv.getEnv();
-        $scope.severityPieChartData($scope.fromDate, $scope.toDate, $scope.env);
+        $scope.severityPieChartData($scope.fromDate, $scope.toDate);
     })
-    $scope.severityPieChartData = function(fromDate, toDate, env){
-        var sliderDataQuery = "[{'$match':{'$and':[{'timestamp':{'$gte':{'$date':'"+$scope.fromDate+"'},'$lt':{'$date':'"+
-            $scope.toDate+"'}}},{'$and':[{'severity':{'$ne':null}}, {'severity':{'$exists':true,'$ne':''}},{'envid':'"+
-            $scope.env.dbName+"'}]}]}},{$group:{_id:'$severity', count:{$sum:1}}}]";  
+    $scope.severityPieChartData = function(fromDate, toDate){
+        var sliderDataQuery = "[{\"$match\":{\"$and\":[{\"timestamp\":{\"$gte\":{\"$date\":\""+fromDate+"\"},\"$lt\":{\"$date\":\""+
+            toDate+"\"}}},{\"$and\":[{\"severity\":{\"$ne\":null}}, {\"severity\":{\"$exists\":true,\"$ne\":\"\"}},{\"envid\":\""+
+            $scope.env.dbName+"\"}]}]}},{$group:{_id:\"$severity\", count:{$sum:1}}}]";  
         $scope.severityPieChartPromise = mongoAggregateService.callHttp(sliderDataQuery);
     };
 }]);
