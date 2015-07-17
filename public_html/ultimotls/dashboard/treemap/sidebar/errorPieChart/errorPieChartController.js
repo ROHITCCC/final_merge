eed58ffaa32fd632141efe5f4900914b9239c4ff
Ultimo User <Ultimo User@Ultimo-Angular>
2015-07-17 13:50:13 -0700

    Initial calls were added so that graphs will appear on the initial load

--- public_html/ultimotls/dashboard/treemap/sidebar/errorPieChart/errorPieChartController.js
+++ public_html/ultimotls/dashboard/treemap/sidebar/errorPieChart/errorPieChartController.js
@@ -1,7 +1,7 @@
-var errorPieChartControllerModule = angular.module('errorPieChartControllerModule', ['ultimotls', 'auditControllerModule']);
+var errorPieChartControllerModule = angular.module('errorPieChartControllerModule', ['ultimotls']);
 
-errorPieChartControllerModule.controller('errorPieChartController', ['$scope', 'mongoAggregateService', 'treemapSaver', 'auditQuery','queryEnv','timeService',
-    function($scope, mongoAggregateService, treemapSaver, auditQuery, queryEnv, timeService){
+errorPieChartControllerModule.controller('errorPieChartController', ['$scope', 'mongoAggregateService', 'treemapSaver' ,'queryEnv','timeService',
+    function($scope, mongoAggregateService, treemapSaver, queryEnv, timeService){
     //service to get current Time
     var time = timeService.getTime();
     $scope.toDate = time.toDate;
@@ -10,24 +10,27 @@ errorPieChartControllerModule.controller('errorPieChartController', ['$scope', '
     $scope.env = queryEnv.getEnv();
     
     $scope.treemapSaver = treemapSaver;
-    $scope.auditQuery = auditQuery; //Don't think i need it, if not remove from injection
-    
-//    need a service to pass timeChange
+    var dataQuery = "[{\"$match\":{\"$and\":[{\"timestamp\":{\"$gte\":{\"$date\":\""+$scope.fromDate+"\"},\"$lt\":{\"$date\":\""+
+            $scope.toDate+"\"}}},{\"$and\":[{\"errorType\":{\"$ne\":null}}, {\"errorType\":{\"$exists\":true,\"$ne\":\"\"}},{\"envid\":\""+
+            $scope.env.dbName+"\"}]}]}},{$group:{_id:\"$errorType\", count:{$sum:1}}}]";  
+    $scope.errorPieChartPromise = mongoAggregateService.callHttp(dataQuery);
+    //need a service to pass timeChange
     $scope.$on("timeChangeBroadcast", function(){//Listens for Time Change
         var timeTemp = timeService.getTime();
         $scope.toDate = timeTemp.toDate;
         $scope.fromDate = timeTemp.fromDate;
         $scope.errorPieChartData($scope.fromDate, $scope.toDate);
     })
-    
+    //Listening to the environment change broadcast
     $scope.$on("envChangeBroadcast", function(){//Listens for Environment Change
         $scope.env = queryEnv.getEnv();
         $scope.errorPieChartData($scope.fromDate, $scope.toDate);
     })
+    //Function that will send our promise to the value that our directive is listening for
     $scope.errorPieChartData = function(fromDate, toDate){
-        var sliderDataQuery = "[{\"$match\":{\"$and\":[{\"timestamp\":{\"$gte\":{\"$date\":\""+fromDate+"\"},\"$lt\":{\"$date\":\""+
+        var dataQuery = "[{\"$match\":{\"$and\":[{\"timestamp\":{\"$gte\":{\"$date\":\""+fromDate+"\"},\"$lt\":{\"$date\":\""+
             toDate+"\"}}},{\"$and\":[{\"errorType\":{\"$ne\":null}}, {\"errorType\":{\"$exists\":true,\"$ne\":\"\"}},{\"envid\":\""+
             $scope.env.dbName+"\"}]}]}},{$group:{_id:\"$errorType\", count:{$sum:1}}}]";  
-        $scope.errorPieChartPromise = mongoAggregateService.callHttp(sliderDataQuery);
+        $scope.errorPieChartPromise = mongoAggregateService.callHttp(dataQuery);
     };
 }]);
