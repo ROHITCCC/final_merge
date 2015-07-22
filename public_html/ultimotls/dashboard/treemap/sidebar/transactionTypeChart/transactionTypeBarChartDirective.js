45510dada3518ee630b73a44e5aae54e313b5dad
Ultimo User <Ultimo User@Ultimo-Angular>
2015-07-22 10:42:02 -0700

    Added a function to show which data was use to filter the results

--- public_html/ultimotls/dashboard/treemap/sidebar/transactionTypeChart/transactionTypeBarChartDirective.js
+++ public_html/ultimotls/dashboard/treemap/sidebar/transactionTypeChart/transactionTypeBarChartDirective.js
@@ -2,8 +2,7 @@ var transactionTypeBarChartDirectiveModule = angular.module('transactionTypeBarC
 
 transactionTypeBarChartDirectiveModule.directive('transactionTypeBarChart',['queryFilter', function(queryFilter){
     function updateSize(data){
-        
-        var width = document.getElementById('transactionTypeBarChartDiv').offsetWidth*.9, height = (window.innerHeight*.28);
+        var width = document.getElementById('transactionTypeBarChartDiv').offsetWidth*.9, height = (window.innerHeight*.30);
         if (data === 0){
             d3.select("#transactionTypeBarChart").select("svg").remove();
             var svg = d3.select("#transactionTypeBarChart").append("svg")
@@ -17,19 +16,28 @@ transactionTypeBarChartDirectiveModule.directive('transactionTypeBarChart',['que
         }
         barChart(data, "updateChart");
         return;
+    };
+    function onSelection(d,i){
+        d3.select("#error").selectAll("path").style("opacity", 1)
+        d3.select("#severity").selectAll("path").style("opacity", 1)
+        d3.select("#transactionType").selectAll("rect").style("opacity", 1)
+        
+        d3.select("#error").selectAll("path").style("opacity", 0.3)
+        d3.select("#severity").selectAll("path").style("opacity", 0.3)
+        d3.select("#transactionType").selectAll("rect").style("opacity", 0.3)
+        
+        d3.select("#transactionTypeBar"+i).style("opacity",1)
     }
     function barChart(data, status){
         if(data.length){
             var dynamicSize = data.length/5;
             var adjustedWidth = dynamicSize *.5 + 1;
-            console.log(adjustedWidth);
         }
         var width = document.getElementById('transactionTypeBarChartDiv').offsetWidth*adjustedWidth, height = (window.innerHeight*.28);
         var width2 = document.getElementById('transactionTypeBarChartDiv').offsetWidth;
-        var color = d3.scale.category20();
+        var color = d3.scale.category10();
         var barChart = {};
         function upDateTreemap(filterCriteria){
-            console.log(filterCriteria)
             queryFilter.appendQuery("transactionType",filterCriteria._id);
             queryFilter.broadcast();
         };
@@ -72,9 +80,9 @@ transactionTypeBarChartDirectiveModule.directive('transactionTypeBarChart',['que
       
             svg.selectAll(".bar").data(data)
                 .enter().append("rect")
-                .on("click", function(d){upDateTreemap(d);})
+                .on("click", function(d,i){upDateTreemap(d);onSelection(d,i)})
                 .style("fill", function(d,i){return color(i);})
-                .attr("class", "bar")
+                .attr("class", "bar").attr("id", function(d,i){return "transactionTypeBar"+i;})
                 //.attr("x", function(d) { return x(d._id)+5; })
                 .attr("x", function(d){return x(d._id)+5;})
                 .attr("width", x.rangeBand())
@@ -93,8 +101,9 @@ transactionTypeBarChartDirectiveModule.directive('transactionTypeBarChart',['que
         };
         if(status === "no_data"){ //Will append a Message for no data and return out of the function
             d3.select("#transactionTypeBarChart").select("svg").remove();
-            var svg = d3.select("#transactionTypeBarChart").append("svg").attr("width", width).attr("height", height).attr("id", "transactionTypeDiv");
-            svg.append("g").attr("transform", "translate(" + width*.065 + "," + height*.5 + ")")
+            var svg = d3.select("#transactionTypeBarChart").append("svg")
+                .attr("id", "transactionTypeDiv").attr("width", width2).attr("height", height)
+                .append("g").attr("transform", "translate(" + width2*.065 + "," + height*.5 + ")")
                 .append("text").text("No Data Available");
             return;
         };
