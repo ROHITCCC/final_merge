1b14a6f9b54a759ddee2df539d52b11bb9e90625
David Donangelo <david.donangelo@gmail.com>
2015-07-23 10:47:51 -0700

    Added date label for custom date usage

--- public_html/ultimotls/dashboard/treemap/treemapController.js
+++ public_html/ultimotls/dashboard/treemap/treemapController.js
@@ -80,6 +80,9 @@ treemapControllerModule.controller('treemapController', ['$scope', '$location', 
                     "{'$date': '"+$scope.fromDate+"'}, '$lt': {'$date': '"+ $scope.toDate +"'} } }, { '$and': [ {'severity': {'$ne': null}}, {'severity': {'$exists': true, '$ne': ''}},{'envid':'"+$scope.env.name+"'} ] } ] } },{ '$group': { '_id' : { 'interface1': '$interface1', 'application': '$application' }, 'count': {'$sum': 1} } } , { '$group': { '_id' : { 'application': '$_id.application' }, 'data': { '$addToSet':{ 'name': '$_id.interface1', 'size': '$count' } } } } , { '$project': { '_id': 1, 'name': '$_id.application', 'children': '$data' } } ]";
             
         $scope.treemapPromise = mongoAggregateService.callHttp(customDateQuery);
+        $scope.fromDate = new Date(fromDate).toDateString().substr(4);
+        $scope.toDate = new Date(toDate).toDateString().substr(4);
+        document.getElementById("customDateTimes").innerHTML = $scope.fromDate + " - " + $scope.toDate;
     }
     $scope.fromDateChange = function(time){
         $scope.timeSelected = time;
@@ -99,5 +102,6 @@ treemapControllerModule.controller('treemapController', ['$scope', '$location', 
             
         $scope.treemapPromise = mongoAggregateService.callHttp(sliderDataQuery);
         $scope.treemapSaver.dropdownVal = $scope.timeSelected.time;//Saves TimeSelected when drop down value changes
+        document.getElementById("customDateTimes").innerHTML = ""
     };
     }]);
