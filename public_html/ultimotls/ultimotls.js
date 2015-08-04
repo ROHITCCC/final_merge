4963cf2471455efdee37642a905cc06107bb7392
David Donangelo <david.donangelo@gmail.com>
2015-08-04 09:34:21 -0700

    fixed file extension format

--- public_html/ultimotls/ultimotls.js
+++ public_html/ultimotls/ultimotls.js
@@ -4,15 +4,15 @@
  * and open the template in the editor.
  */
 //GLOBAL VARIABLES FOR INTITIAL SETUP
-var TLS_PROTOCOL = "http";
-var TLS_SERVER = "172.16.120.157";
-var TLS_PORT = "8080";
-var TLS_DBNAME = "ES";
-var TLS_SERVER_TIMEOUT = 6000;
-//var TLS_PROTOCOL = location.protocol.replace(/[:]/g , '');
-//var TLS_SERVER = location.hostname;
-//var TLS_PORT = location.port;
+//var TLS_PROTOCOL = "http";
+//var TLS_SERVER = "172.16.120.157";
+//var TLS_PORT = "8080";
+//var TLS_DBNAME = "ES";
 //var TLS_SERVER_TIMEOUT = 6000;
+var TLS_PROTOCOL = location.protocol.replace(/[:]/g , '');
+var TLS_SERVER = location.hostname;
+var TLS_PORT = location.port;
+var TLS_SERVER_TIMEOUT = 6000;
    
 //(function(angular){
 var ultimotls = angular.module('ultimotls', ['auditControllerModule', 'auditDirectiveModule' , 'treemapDirectiveModule', 'base64', 
